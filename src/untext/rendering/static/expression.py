374c3ced140df3eb3ae70ca976dee9244b5a66cd
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2025-12-01 13:50:10 +0100

    cleanup

--- src/untext/rendering/static/expression.py
+++ src/untext/rendering/static/expression.py
@@ -1,6 +1,52 @@
 """
 expression rendering
+
+from the official python documentation:
+https://docs.python.org/3/library/ast.html
+(at the time of writing, for python 3.14)
+
+
+expr = BoolOp(boolop op, expr* values)
+    | NamedExpr(expr target, expr value)
+    | BinOp(expr left, operator op, expr right)
+    | UnaryOp(unaryop op, expr operand)
+    | Lambda(arguments args, expr body)
+    | IfExp(expr test, expr body, expr orelse)
+    | Dict(expr?* keys, expr* values)
+    | Set(expr* elts)
+    | ListComp(expr elt, comprehension* generators)
+    | SetComp(expr elt, comprehension* generators)
+    | DictComp(expr key, expr value, comprehension* generators)
+    | GeneratorExp(expr elt, comprehension* generators)
+    -- the grammar constrains where yield expressions can occur
+    | Await(expr value)
+    | Yield(expr? value)
+    | YieldFrom(expr value)
+    -- need sequences for compare to distinguish between
+    -- x < 4 < 3 and (x < 4) < 3
+    | Compare(expr left, cmpop* ops, expr* comparators)
+    | Call(expr func, expr* args, keyword* keywords)
+    | FormattedValue(expr value, int conversion, expr? format_spec)
+    | Interpolation(expr value, constant str, int conversion, expr? format_spec)
+    | JoinedStr(expr* values)
+    | TemplateStr(expr* values)
+    | Constant(constant value, string? kind)
+
+    -- the following expression can appear in assignment context
+    | Attribute(expr value, identifier attr, expr_context ctx)
+    | Subscript(expr value, expr slice, expr_context ctx)
+    | Starred(expr value, expr_context ctx)
+    | Name(identifier id, expr_context ctx)
+    | List(expr* elts, expr_context ctx)
+    | Tuple(expr* elts, expr_context ctx)
+
+    -- can appear only in Subscript
+    | Slice(expr? lower, expr? upper, expr? step)
+
+    -- col_offset is the byte offset in the utf8 string the parser uses
+    attributes (int lineno, int col_offset, int? end_lineno, int? end_col_offset)
 """
+
 
 from webview.dom.element import Element
 import ast
@@ -9,7 +55,8 @@ import html
 import json
 
 #from untext.rendering.html import register, div, add_node, add, add_text, add_pre
-from .html import node, text, element
+from .html import node, text, element, debug, register_node
+from . import html
 
 
 def render(node: ast.expr):
@@ -76,46 +123,6 @@ def render(node: ast.expr):
             raise ValueError(f"Unexpected ast expression type: {type(node)}")
 
 
-
-
-#BoolOp(boolop op, expr* values)
-#NamedExpr(expr target, expr value)
-#BinOp(expr left, operator op, expr right)
-#UnaryOp(unaryop op, expr operand)
-#Lambda(arguments args, expr body)
-#IfExp(expr test, expr body, expr orelse)
-#Dict(expr?* keys, expr* values)
-#Set(expr* elts)
-#ListComp(expr elt, comprehension* generators)
-#SetComp(expr elt, comprehension* generators)
-#DictComp(expr key, expr value, comprehension* generators)
-#GeneratorExp(expr elt, comprehension* generators)
-## the grammar constrains where yield expressions can occur
-#Await(expr value)
-#Yield(expr? value)
-#YieldFrom(expr value)
-## need sequences for compare to distinguish between
-## x < 4 < 3 and (x < 4) < 3
-#Compare(expr left, cmpop* ops, expr* comparators)
-#Call(expr func, expr* args, keyword* keywords)
-#FormattedValue(expr value, int conversion, expr? format_spec)
-#Interpolation(expr value, constant str, int conversion, expr? format_spec)
-#JoinedStr(expr* values)
-#TemplateStr(expr* values)
-#Constant(constant value, string? kind)
-#
-## the following expression can appear in assignment context
-#Attribute(expr value, identifier attr, expr_context ctx)
-#Subscript(expr value, expr slice, expr_context ctx)
-#Starred(expr value, expr_context ctx)
-#Name(identifier id, expr_context ctx)
-#List(expr* elts, expr_context ctx)
-#Tuple(expr* elts, expr_context ctx)
-#
-#- can appear only in Subscript
-#Slice(expr? lower, expr? upper, expr? step)
-#
-#
 
 def render_boolop(node: ast.BoolOp):
     yield from element("bg-red", text("boolop"))
