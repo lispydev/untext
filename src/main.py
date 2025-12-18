538c3d28750be5987605b9661a3825ffc2d2b317
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2025-12-18 10:59:44 +0100

    (wip) successfully build in docker

--- src/main.py
+++ src/main.py
@@ -2,6 +2,13 @@
 main entrypoint when built with pyinstaller
 """
 
+# in the container build process with a python3.13-slim image, python packages are not in the path
+# this line forces imports to be resolvable so that pyinstaller bundles them
+#import sys
+#sys.path.append("/usr/lib/python3/dist-packages")
+#sys.path.insert(0, ".")
+
+
 #equivalent arguments in the pyinstaller build step: --hidden-import pywebview --hidden-import webview --hidden-import code --hidden-import untext.rendering.dynamic.statement --hidden-import untext.rendering.dynamic.expression --hidden-import untext.rendering.static --onefile
 
 import webview
@@ -11,11 +18,12 @@ import untext.rendering.dynamic.statement
 import untext.rendering.dynamic.expression
 import untext.rendering.static
 
+# don't know who uses it, but something did
+#import uuid
+#import ssl # used by pywebview
+#import socketserver
 
-# TODO: remove
-# (the pyinstaller build step would strip the current directory from sys.path)
-#import sys
-#sys.path.insert(0, ".")
+
 from untext.main import main
 
 main()
