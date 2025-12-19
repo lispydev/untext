340c1579a6eb201abeef9cd786a0de8d63b4266c
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2025-12-19 14:11:32 +0100

    fix missing module in packaged build

--- src/main.py
+++ src/main.py
@@ -2,10 +2,7 @@
 main entrypoint when built with pyinstaller
 """
 
-# in the container build process with a python3.13-slim image, python packages are not in the path
-# this line forces imports to be resolvable so that pyinstaller bundles them
 #import sys
-#sys.path.append("/usr/lib/python3/dist-packages")
 #sys.path.insert(0, ".")
 
 
@@ -14,14 +11,8 @@ main entrypoint when built with pyinstaller
 import webview
 import webview
 import code
-import untext.rendering.dynamic.statement
-import untext.rendering.dynamic.expression
+import untext.rendering.dynamic
 import untext.rendering.static
-
-# don't know who uses it, but something did
-#import uuid
-#import ssl # used by pywebview
-#import socketserver
 
 
 from untext.main import main
